cbfee39d4e59fb0f080576c497a1a05ac690cf0c
adriellison <adriellisonki@gmail.com>
2022-07-01 00:13:00 -0300

    novos desafios

---
+++ Desafios Iniciais JavaScript/desafio4.js
@@ -0,0 +1,15 @@
+// a função gets é implementada dentro do sistema para ler as entradas(inputs) dos dados e a função print para imprimir a saída (output) de dados e já pula uma linha ("\n")
+// Abaixo segue um exemplo de código que você pode ou não utilizar
+
+linha = '---------------------------------------';
+pipe =  '|                                     |'
+
+console.log(linha);
+for(i = 0; i < 5; i++){
+    console.log();
+    console.log(pipe);
+}
+console.log();
+console.log(linha);
+
+//TODO: Complete os espaços em branco com uma possível solução para o desafio
